8e9b047c24559b072dc972becba74a4b1297a04c
MartijnvDijk <m.van.dijk@nijmegen.nl>
2025-10-22 17:37:07 +0200

    chore: shared npm tp

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,7 +6,6 @@ const project = new GemeenteNijmegenTsPackage({
   name: '@gemeentenijmegen/middy',
   projenrcTs: true,
   repository: 'https://github.com/GemeenteNijmegen/modules-middy.git',
-  npmTrustedPublishing: true,
   deps: [
     '@middy/core',
     '@gemeentenijmegen/utils',
